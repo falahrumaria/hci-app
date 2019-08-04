7bc40f2d69ce2bfa9d3f0adc0b45d6d4eb24ef2f
falahrumaria <rumaria.falah@gmail.com>
2019-08-05 05:24:45 +0700

    gh

---
+++ src/main/java/com/hci/test/hciapp/HciAppApplication.java
@@ -0,0 +1,13 @@
+package com.hci.test.hciapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HciAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HciAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hci/test/hciapp/HciAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hci.test.hciapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HciAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
